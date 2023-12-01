afabdae5ddc8ae3f8b11669db086baac08e14740
MirolieTheroux <mirolie010@gmail.com>
2023-12-01 14:41:26 -0500

    affichage de l'employé (zoom) à partir de la page projet

---
+++ GestionProjetsEtClients/InfosNavigation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionProjetsEtClients
+{
+    public class InfosNavigation
+    {
+
+        public string NomPage { get; set; }
+        public string MatEmploye { get; set; }
+        public int IndexEmploye { get; set; }
+    }
+}
